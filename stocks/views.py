3565d2f9a8be23513716693cad38f04885756558
MamunurRahmanMoon <mamunurrahmanmoon@gmail.com>
2024-01-16 23:12:44 +0600

    line chart

--- stocks/views.py
+++ stocks/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Stock
-import json
+
+import pandas as pd
 
 def home(request):
     # Retrieve all data from database
     stocks = Stock.objects.all()
 
-    return render(request, 'home.html', {'stocks': stocks})
+    queryset = Stock.objects.values('date', 'close')
+    df = pd.DataFrame.from_records(queryset)
+    df['date'] = pd.to_datetime(df['date'])
+    df.sort_values('date', inplace=True)
+
+    data = {
+        'labels': df['date'].dt.strftime('%Y-%m-%d').tolist(),
+        'close_prices': df['close'].tolist(),
+    }
+
+    return render(request, 'home.html', {'stocks': stocks, 'data': data})
 
 
 @csrf_exempt
@@ -33,3 +44,19 @@ def delete_stock(request, stock_id):
         return JsonResponse({'status': 'success'})
     else:
         return JsonResponse({'status': 'error'})
+    
+
+
+# def getChart(request):
+#     queryset = Stock.objects.values('date', 'close')
+#     df = pd.DataFrame.from_records(queryset)
+#     df['date'] = pd.to_datetime(df['date'])
+#     df.sort_values('date', inplace=True)
+
+#     data = {
+#         'labels': df['date'].dt.strftime('%Y-%m-%d').tolist(),
+#         'close_prices': df['close'].tolist(),
+#     }
+
+#     chart_html = render(request, 'chart.html', {'data': data})
+#     return HttpResponse(chart_html)
